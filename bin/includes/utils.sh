583a83a05632c69912519b86d5dc6f19a91112c3
Henrik Farre <henrik@bellcom.dk>
2016-04-11 20:00:03 +0200

    site-create support for creating drupal 7/8 sites

--- bin/includes/utils.sh
+++ bin/includes/utils.sh
@@ -94,7 +94,7 @@ function usage() {
   echo ""
   echo "Avaliable actions:"
   echo -e "\tstart\t\t: Starts a container, see container dir for avaliable containers"
-  echo -e "\tsite-create\t: Creates a virtual host in $VHOST_DIR and directory structure in $SITE_DIR"
+  echo -e "\tsite-create [TYPE]\t: Creates a virtual host in $VHOST_DIR and directory structure in $SITE_DIR\nTYPE is optional, but can be one of: Drupal7, Drupal8"
   echo -e "\tdb-import\t: Imports a MySQL database dump. Note: be sure not to overwrite existing databases"
   echo -e "\tapache-reload\t: Restarts Apache in the container, for example to load a new virtual host"
   echo -e "\tshell\t\t: Executes an interactive shell inside the container"
@@ -103,7 +103,9 @@ function usage() {
 
 function site-create() {
   local SITE_NAME
+  local SITE_TYPE
   SITE_NAME=$1
+  SITE_TYPE=${2:-}
   if [[ -d "$SITE_DIR/$SITE_NAME" ]]; then
     msg_error "$SITE_DIR/$SITE_NAME exists"
     exit 1
@@ -114,9 +116,69 @@ function site-create() {
     exit 1
   fi
 
-  mkdir -p "${SITE_DIR}/${SITE_NAME}"/{public_html,logs,sessions,upload}
-  sed -re "s/\[DOMAIN.TLD\]/$SITE_NAME/" bin/includes/vhost.tpl > "$VHOST_DIR/${SITE_NAME}.conf"
+  site-create-dirs "$SITE_NAME"
+  site-create-vhost "$SITE_NAME"
+  if [[ ! -z "$SITE_TYPE" ]]; then
+    local CONTAINER_NAME
+    CONTAINER_NAME=$(docker-get-running-container-name)
+
+    case "${SITE_TYPE^^}" in
+      DRUPAL7)
+        docker-exec-in-container "$CONTAINER_NAME" site-create-drupal "$SITE_NAME"
+        ;;
+      DRUPAL8)
+        docker-exec-in-container "$CONTAINER_NAME" site-create-drupal "$SITE_NAME" "8"
+        ;;
+    esac
+  fi
   apache-reload
+}
+
+function site-create-drupal() {
+    local SITE_NAME
+    local DRUPAL_VERSION
+    local DATABASE_NAME
+    SITE_NAME=${1}
+    # Default to version 7
+    DRUPAL_VERSION=${2:-7}
+    # Lowercase and replace " " and . with _
+    DATABASE_NAME=${SITE_NAME,,}
+    DATABASE_NAME=${DATABASE_NAME//./_}
+    DATABASE_NAME=${DATABASE_NAME// /_}
+    cd "/var/www/${SITE_NAME}/"
+    rmdir public_html
+    msg_info "Creating Drupal ${DRUPAL_VERSION} site"
+    drush dl "drupal-${DRUPAL_VERSION}" --drupal-project-rename=public_html -y
+    cd public_html
+    drush site-install standard --account-name=admin --account-pass=admin --db-url=mysql://root:${MYSQL_PASS}@localhost/"${DATABASE_NAME}" --site-name="$SITE_NAME" -y
+    case $DRUPAL_VERSION in
+      7)
+        drush -y vset file_temporary_path "/var/www/${SITE_NAME}/tmp"
+        ;;
+      8)
+        drush -y config-set system.file path.temporary "/var/www/${SITE_NAME}/tmp/"
+    esac
+}
+
+function site-create-vhost() {
+  local SITE_NAME
+  SITE_NAME=$1
+  sed -re "s/\[DOMAIN.TLD\]/$SITE_NAME/" "${WORK_DIR}/bin/includes/vhost.tpl" > "${VHOST_DIR}/${SITE_NAME}.conf"
+}
+
+function site-create-dirs() {
+  local SITE_NAME
+  SITE_NAME=$1
+  mkdir -p "${SITE_DIR}/${SITE_NAME}"/{public_html,logs,sessions,upload,tmp}
+}
+
+function site-fix-permissions() {
+  msg_info "Setting group to www-data, rwX permissions and group sticky bit on dirs"
+  local SITE_NAME
+  SITE_NAME=$1
+  chgrp -R www-data "/var/www/${SITE_NAME}"
+  chmod -R g+rwX "/var/www/${SITE_NAME}"
+  find "/var/www/${SITE_NAME}" -type d -exec chmod g+s "{}" \;
 }
 
 function apache-reload() {
